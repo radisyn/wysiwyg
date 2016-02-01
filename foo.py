7980ca7bfe85688652c321a7810bcce8019a237a
Madisyn Schultz <mad@MacBookAirborne.local>
2016-01-31 21:25:40 -0800

    Add comments to foo.py

--- foo.py
+++ foo.py
@@ -1,12 +1,16 @@
 import turtle
 def draw_square():
+    # Setup the window
     window = turtle.Screen()
     window.bgcolor("red")
+
+    # Create a turtle
     mad = turtle.Turtle()
     mad.shape("turtle")
     mad.color("yellow")
     mad.speed(2)
 
+    # Draw the square
     mad.forward(100)
     mad.right(90)
     mad.forward(100)
@@ -16,9 +20,12 @@ def draw_square():
     mad.forward(100)
     mad.right(90)
 
+    # Create another turtle
     gilbert = turtle.Turtle()
     gilbert.shape("arrow")
     gilbert.color("blue")
+
+    # Draw a circle
     gilbert.circle(100)
 
     window.exitonclick()
